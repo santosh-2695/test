03c5bf05776295f88e05e1b864b8c452f9e6ef0d
Santosh More <santosh.shidory@gmail.com>
2021-06-26 17:09:58 +0530

    First Commit
    Uploading all files

---
+++ Test/src/com/code/example/Addition.java
@@ -0,0 +1,21 @@
+package com.code.example;
+
+public class Addition {
+	
+	static int a,b,c;
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		a = 100;
+		b = 200;
+		
+		c = a + b;
+		
+		System.out.println("Addition is:"+c);
+		
+		
+
+	}
+
+}
